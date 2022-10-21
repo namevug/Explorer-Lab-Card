a80a06c4db9e90a75ce7bbbe325afe1658976199
namevug <ciavug@gmail.com>
2022-10-21 19:31:18 -0300

    feat: ex/cvc

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
@@ -9,9 +11,35 @@ function setCardType(type) {
     mastercard: ["#DF6F29", "#C69347"],
     default: ["black", "gray"],
   }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-ccBgColor01.setAttribute("fill", colors[type][0])
-ccBgColor02.setAttribute("fill", colors[type][1])
+globalThis.setCardType = setCardType
 
-setCardType("default")
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 15).slice(2),
+    },
+
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
